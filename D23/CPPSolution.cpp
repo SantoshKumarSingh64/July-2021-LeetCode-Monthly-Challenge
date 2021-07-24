eaf4f1ef586d997584f594b1ed61b060231a4614
Santosh Kumar Singh <ss7257711@gmail.com>
2021-07-24 09:55:24 +0530

    C++ Solution

---
+++ D23/CPPSolution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* node) {
+        if(!node)
+            return NULL;
+        
+        node->left = pruneTree(node->left);
+        node->right = pruneTree(node->right);
+        
+        if(!node->left && !node->right && !node->val){
+            return NULL;
+        }
+        
+        return node;
+    }
+};
