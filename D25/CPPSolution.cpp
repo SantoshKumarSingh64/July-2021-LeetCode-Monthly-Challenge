2b11289f96c3efd8e91ab1f7e77240a8976b7b6d
Santosh Kumar Singh <ss7257711@gmail.com>
2021-07-26 08:58:38 +0530

    C++ Solution

---
+++ D25/CPPSolution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findIntegers(int n) {
+        vector<int>f(31);
+        f[0] = 1;
+        f[1] = 2;
+        
+        for(int i=2;i<31;i++){
+            f[i] = f[i-1]+f[i-2];
+        }
+        
+        int ans = 0;
+        int k = 30;
+        int pre_bit = 0;
+        
+        while(k >= 0){
+            if(n & (1<<k)){
+                ans += f[k];
+                if(pre_bit)
+                    return ans;
+                pre_bit = 1;
+            }
+            else{
+                pre_bit = 0;
+            }
+            k--;
+        }
+        return ans+1;
+    }
+};
